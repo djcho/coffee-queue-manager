78e71eb9fcd85ef7bf69e9ae1b234774d28e36da
Daejoon Cho <eowns36@gmail.com>
2024-07-03 15:44:06 +0900

    Update app.py

--- app.py
+++ app.py
@@ -51,5 +51,5 @@ def coffee_queue_handler():
     return jsonify(response)
 
 if __name__ == "__main__":
-    app.run()
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
