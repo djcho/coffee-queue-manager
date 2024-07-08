a6e736e0dfa6398cd59bfc887fba6a64e25aa915
admin <admin@adminui-MacBookPro-2.local>
2024-07-08 17:57:30 +0900

    큐 내용 지웠을 때 로그에 사유 출력

--- app.py
+++ app.py
@@ -140,10 +140,8 @@ def coffee_queue_handler():
                 db.session.delete(user_to_remove)
                 db.session.commit()
                 adjust_order_after_remove(index)
-
-                delete_user = user_to_remove.username
-                log_action("remove", delete_user)
-                message = f"{delete_user}님이 큐에서 제거되었습니다.\n현재 큐:\n{get_queue_list()}"
+                log_action("remove", user_to_remove.username, user_to_remove.reason)
+                message = f"{user_to_remove.username}님이 큐에서 제거되었습니다.\n현재 큐:\n{get_queue_list()}"
             else:
                 message = "잘못된 인덱스입니다. 유효한 인덱스를 입력하세요."
         except (ValueError, IndexError):
